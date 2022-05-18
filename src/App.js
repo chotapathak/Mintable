cfc50e6c0c1e6cb23ef7e1333f3ec1e86eee97e7
Vikash Pathak <chotapathak12@gmail.com>
2022-05-19 00:42:36 +0530

    api and frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
+import './App.css';
+import SearchPhotos from "./searchPhotos"
+
+import Main from './Main';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+ <div className="container">
+        <h1 className="title">Looking for NFT?</h1>
+        <SearchPhotos /> 
+        <Main/>
+        </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/Main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './main.css';
+
+function Main() {
+  return (
+    <div id="home">
+      <div className="content">
+        <h1>Are.</h1>
+        <h1> You working on </h1>
+        <h1>
+          Collection
+        </h1>
+        <div className="fields">
+          <h4>Bitcoin</h4>
+          <h4>Etherum</h4>
+          <h4>Cardano &amp; trader</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
